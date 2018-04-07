a2a4b6a70600b4e9b4757fa0029fb40f31161275
Rand al'Thor <RandyTaschner@Randels-MacBook-Pro.local>
2018-04-07 11:15:23 -0600

    First commit, please

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Infinite Retries</h1>
+          <h1 className="App-title">Infinite Retries</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
